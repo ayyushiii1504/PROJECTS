d2b93ed2424675de7fd3505909a91779acf05070
Ayushi Singh <singhayushi801@gmail.com>
2025-04-11 12:56:31 +0530

    updated few files

--- ai-code-debugger/ai-code-debugger-frontend/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Footer = () => {
-  return (
-    <footer className="footer">
-      <p> © 2025 AI Code Debugger. All rights reserved.</p>
-    </footer>
-  );
-};
